c179429875074203dad02516e5beef52bd7a9706
leipazapalli <leipazapalli@paypal.com>
2013-10-11 14:35:05 -0700

    register

--- mvc/controller/index.js
+++ mvc/controller/index.js
@@ -3,48 +3,38 @@
 module.exports = function (server) {
     'use strict';
 
-    /**
-     * The default route for this application: /
-     * @param {Object} req the HTTP request object
-     * @param {Object} res the HTTP response object
-     */
-    // server.get('/', function (req, res) {
-    //     var model = {
-    //             viewName: "hello",
-    //             baseTemplate: 'master',
-    //             data: {
-    //                 title: 'hello Sai'
-    //             }
-    //         };
-    //         res.render("public/templates/" + model.baseTemplate, model);
-    // });
 
     server.get('/', function (req, res) {
-
-
-    req.model = {
-        viewName: 'index',
-        master: 'public/templates/master',
-        data: {
-            title: 'home page'
-        }
-
-    };
-
-    res.render(req.model.master, req.model);
-
-});
-
-    server.get('/contact', function (req, res) {
-        var model = {viewName: 'contact'};
-        res.render('contact', model);
+        req.model = {
+            viewName: 'index',
+            master: 'public/templates/master',
+            data: {
+                title: 'home page'
+            }
+        };
+        res.render(req.model.master, req.model);
     });
-     server.get('/about', function (req, res) {
-        var model = {viewName: 'about'};
-        res.render('about', model);
+
+    server.get('/register', function (req, res) {
+        req.model = {
+            viewName: 'register/register',
+            master: 'public/templates/master',
+            data: {
+                title: 'Register'
+            }
+        };
+        res.render(req.model.master, req.model);
     });
-     server.get('/landing', function (req, res) {
-        var model = {viewName: 'landing'};
-        res.json('landing', model);
+
+    server.get('/register/:id', function (req, res) {
+        req.model = {
+            viewName: 'register/'+req.params.id,
+            master: 'public/templates/master',
+            data: {
+                title: 'Register'
+            }
+        };
+        res.render(req.model.master, req.model);
     });
+     
 };
