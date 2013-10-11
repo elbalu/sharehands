47260a25b0f6c789ca6ad78c72e167d03340c9a1
Abilash Badri <abadri@paypal.com>
2013-10-11 12:44:21 -0700

    Adding dummy controllers

--- mvc/controller/index.js
+++ mvc/controller/index.js
@@ -36,15 +36,46 @@ module.exports = function (server) {
 });
 
     server.get('/contact', function (req, res) {
-        var model = {viewName: 'contact'};
-        res.render('contact', model);
+        req.model = {
+        viewName: 'contact',
+        master: 'public/templates/master',
+        data: {
+            title: 'contact page'
+        }
+
+    };
+     res.render(req.model.master, req.model);
+
     });
      server.get('/about', function (req, res) {
         var model = {viewName: 'about'};
         res.render('about', model);
     });
-     server.get('/landing', function (req, res) {
+    
+    server.get('/landing', function (req, res) {
         var model = {viewName: 'landing'};
         res.json('landing', model);
     });
+
+    
+    server.get('/getPost', function(req, res) {
+        var model = {id:req.params.id, name: "Send all the list of adds in the region", description: "Send the all add post", status: 'sucess', viewName: 'sucess'};
+        res.json('sucess', model);
+    });
+
+    server.get('/getPost/:id', function(req, res) {
+        var model = {id:req.params.id, name: "Get the post", description: "Send the particular add post", status: 'sucess', viewName: 'sucess'};
+        res.json('sucess', model);
+    });
+
+    server.get('/register/:type', function(req, res) {
+        var model = {type:req.params.type, name: "Registration module", description: "Acoount creation is sucess", viewName: 'sucess'};
+        res.json('sucess', model);
+    });
+
+    server.get('/map/:id', function(req, res) {
+        var model = {id:req.params.id, name: "The Name", description: "description"};
+        res.json('map', model);
+    });
+
 };
