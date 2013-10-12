dda8e1da6fe78d28f3a3508f47f76d3522f1aa7a
rvaidyalingam <rvaidyalingam@corp.ebay.com>
2013-10-12 10:33:12 -0700

    Marker link now opens in lightbox

--- public/js/views/getMapPost.js
+++ public/js/views/getMapPost.js
@@ -71,16 +71,23 @@ define([
 					      position: new google.maps.LatLng(x[i].latitude,x[i].longitude),
 					      map: map, 
 					      title: x[i].title,
-					      icon: x[i].type == "have" ? "/img/haveMarker.png" : "/img/wantMarker.png"
+					      icon: x[i].type == "have" ? "/img/haveMarker.png" : "/img/wantMarker.png",
+					      desc: x[i].desc,
+					      email: x[i].email,
+					      recType: x[i].type
 						  });
 					  	attachMessage(marker, i, x[i].desc, x[i]._id);
 					}
 
 					function attachMessage(marker, num, message, id){
 						var infoWindow = new google.maps.InfoWindow({
-				    		content: "<a href='/getPost/"+ id +"'>" + message + "</a>"
+				    		//content: "<a data-toggle='modal' href='/getPost/"+ id +"'>" + message + "</a>"
+				    		content: "<a data-toggle='modal' href='#myModal'>" + message + "</a>"
 						});
 					  	google.maps.event.addListener(marker, 'click', function () {
+					  		$("#modalTitle").html(marker.title);
+					  		$("#modalBody").html("<p><strong>Detailed Information: </strong>"+marker.desc+"</p><p><strong>Email Address: </strong>"+marker.email+"</p>");
+					  		$("#modalButton").html(marker.recType == "have" ? "This will help me" : "I will help");
 							infoWindow.open(marker.get('map'), marker);
 						});
 					}
